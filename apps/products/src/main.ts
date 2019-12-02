55edc1053c529357afc0140ff3cbcea49c4c249a
TmTron <martin.trummer@tmtron.com>
2019-12-02 08:29:45 +0100

    added lint violation

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -3,9 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {products} from "@nx-example/shared/product/data";
 
 if (environment.production) {
-  enableProdMode();
+  /**
+   * FIXME: explicitly added a dependency to shared/product/data
+   *        so that the nx lint check should report a violation
+   *
+   * ERROR: D:/dev_no_backup/nx-examples/apps/products/src/main.ts:6:1 - A project tagged with "type:app" can only
+   *        depend on libs tagged with "type:feature", "type:ui"
+   */
+  if (products) enableProdMode();
 }
 
 platformBrowserDynamic()
